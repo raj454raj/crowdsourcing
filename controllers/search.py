a9557d5bf998576d7c2a1e08f228c45d8da986bd
Abhineet Jain <abhiineitjain@gmail.com>
2015-04-14 04:21:54 +0530

    Twitter, IG search done.

--- controllers/search.py
+++ controllers/search.py
@@ -1,9 +1,10 @@
 # -*- encoding: utf-8 -*-
 from __future__ import unicode_literals
-import requests
+import requests, urllib
 from requests_oauthlib import OAuth1
 from pytz import timezone
 from datetime import datetime
+import HTMLParser
 
 REQUEST_TOKEN_URL = "https://api.twitter.com/oauth/request_token"
 AUTHORIZE_URL = "https://api.twitter.com/oauth/authorize?oauth_token="
@@ -17,6 +18,8 @@ OAUTH_TOKEN_SECRET = "16Tj3u1OMCln4gqGP1al02CV82fx930FGznjgHct6Vp4h"
 
 proxy = {"http": "http://proxy.iiit.ac.in:8080", "https" : "https://proxy.iiit.ac.in:8080"}
 
+INSTAGRAM_ACCESS_TOKEN = "1833988825.999e36e.a3efced35bd1407d9a7719390da9255b"
+
 def __get_oauth__():
     oauth = OAuth1(CONSUMER_KEY,
     client_secret=CONSUMER_SECRET,
@@ -28,19 +31,27 @@ def index():
     ist = timezone('Asia/Kolkata')
     utc = timezone('UTC')
     tweet_list = []
+    instagram_list = []
     search_query = None
     if request.vars:
+        network = request.vars['network']
         search_query = request.vars['search']
-        url = "https://api.twitter.com/1.1/search/tweets.json?q="+search_query+"&count=100"
-        oauth = __get_oauth__()
-        r = requests.get(url = url, auth = oauth, proxies = proxy)
-        r = r.json()
-        tweet_list = r['statuses']
-    form = FORM(TABLE(TR(TD(), TD(INPUT(_name="search", _placeholder="Search Twitter")), TD(INPUT(_type="submit", _value="Search", _class="btn btn-info")))), _method="GET")
-    t = TABLE(_class="table")
-    tweet_counter = 0
+        if network == "twitter":
+            url = "https://api.twitter.com/1.1/search/tweets.json?q="+urllib.quote(search_query, '')+"&count=100&result_type=mixed"
+            oauth = __get_oauth__()
+            r = requests.get(url = url, auth = oauth, proxies = proxy)
+            r = r.json()
+            tweet_list = r['statuses']
+        elif network == "instagram":
+            if search_query.find(' ') > 0:
+                search_query = search_query[:search_query.find(' ')]
+            url = "https://api.instagram.com/v1/tags/" + search_query + "/media/recent?access_token=" + INSTAGRAM_ACCESS_TOKEN + "&count=100"
+            r = requests.get(url = url, proxies = proxy)
+            r = r.json()
+            instagram_list = r['data']
+    form = FORM(TABLE(TR(TD(INPUT(_name="search", _placeholder="Enter keywords", _value=search_query), _style="vertical-align: middle;"), TD(TAG['button']('Search Twitter ', IMG(_src="static/images/twitter.png"), _type="submit", _value="twitter", _class="btn btn-xs btn-info", _name="network")), TD(TAG['button']('Search Instagram ', IMG(_src="static/images/instagram.png"), _type="submit", _value="instagram", _class="btn btn-xs btn-danger", _name="network")))), _method="GET")
+    t = TABLE(_class="table-striped table-condensed")
     for i in tweet_list:
-        tweet_counter += 1
         username = i['user'].get('screen_name', '')
         media_object = i['entities'].get('media', None)
         created_at = datetime.strptime(i['created_at'], '%a %b %d %H:%M:%S +0000 %Y')
@@ -51,9 +62,32 @@ def index():
         if media_object:
             media_path = media_object[0].get('media_url', '')
             the_image = IMG(_src=media_path, _height=media_object[0]['sizes']['thumb']['h'], _width=media_object[0]['sizes']['thumb']['w'])
+        t.append(TR(TD(IMG(_src=i['user']['profile_image_url']), _style="vertical-align: middle;"), TD(H6(i['user']['name']), A("@"+username, _href="http://twitter.com/"+username))))
         if i.get('retweeted_status', None) is not None:
-            t.append(TR(TD(unicode(tweet_counter)+"."), TD(IMG(_src=i['user']['profile_image_url'])), TD(A("@"+username, _href="http://twitter.com/"+username)), TD(MARKMIN("RT @" + i['retweeted_status']['user']['screen_name'] + ": " + i['retweeted_status']['text']), DIV(the_image, P(ist_created_at, _class="text-muted small")))))
+            tweet = MARKMIN(HTMLParser.HTMLParser().unescape("RT @" + i['retweeted_status']['user']['screen_name'] + ": " + i['retweeted_status']['text']))
+        else:    
+            tweet = MARKMIN(HTMLParser.HTMLParser().unescape(i['text']))
+        t.append(TR(TD(), TD(tweet, DIV(the_image, P(ist_created_at, _class="text-muted small")))))
+    for i in instagram_list:
+        username = i['user']['username']
+        created_at = float(i['created_time'])
+        ist_created_at = datetime.fromtimestamp(created_at).strftime('at %H:%M on %a, %b %d, %Y')
+        t.append(TR(TD(IMG(_src=i['user']['profile_picture']), _style="vertical-align: middle;"), TD(H6(i['user']['full_name']), A("@"+username, _href="http://instagram.com/"+username))))
+        caption = i.get('caption', None)
+        if caption is not None:
+            caption = MARKMIN(HTMLParser.HTMLParser().unescape(caption['text']))
         else:
-            t.append(TR(TD(unicode(tweet_counter)+"."), TD(IMG(_src=i['user']['profile_image_url'])), TD(A("@"+username, _href="http://twitter.com/"+username)), TD(MARKMIN(i['text']), DIV(the_image, P(ist_created_at, _class="text-muted small")))))
+            caption = MARKMIN('')
+        if i['type'] == 'video':
+            image_object = i['images']['low_resolution']
+            video_object = i['videos'].get('low_bandwidth', None)
+            if video_object is None:
+                video_object = i['videos'].get('low_resolution', None)
+            if video_object is None:
+                video_object = i['videos'].get('standard_resolution', None)
+            t.append(TR(TD(), TD(caption, DIV(TAG['video'](_src=video_object['url'], _width=video_object['width'], _height=video_object['height'], _controls="True", _poster=image_object['url']), P(ist_created_at, _class="text-muted small")))))
+        elif i['type'] == 'image':
+            image_object = i['images']['low_resolution']
+            t.append(TR(TD(), TD(caption, DIV(IMG(_src=image_object['url'], _width=image_object['width'], _height=image_object['height']), P(ist_created_at, _class="text-muted small")))))
     return dict(t = t, form = form, search_query = search_query)
     
